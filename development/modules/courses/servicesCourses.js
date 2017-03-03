5042c8542fe8639ad6db8e22aca5286c4ffce352
Marcos J. Alvarez <garusis@gmail.com>
2017-03-03 12:03:33 -0500

    Remove bad implementation of relation

--- development/modules/courses/servicesCourses.js
+++ development/modules/courses/servicesCourses.js
@@ -139,7 +139,7 @@
     this.showModalVideo = function (video) {
       var controller = function (s, e) {
         s.video = video;
-        s.video.getComments();
+        s.video.comments.get();
         console.log(video);
         s.x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         s.callbackvideo = function () {
