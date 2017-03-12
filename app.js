1fd14aafcb00724d1d0c20c45c4c4a24d87d7e94
Marcos J. Alvarez <garusis@gmail.com>
2017-03-12 00:02:27 -0500

    Add favicon

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 
 const express = require("express")
 const path = require("path")
+const compression= require("compression")
+const favicon = require('serve-favicon')
 let app = express()
 
 let dirEnv = process.env.NODE_ENV || "development"
 
+
+app.use(compression())
+app.use(favicon(path.join(__dirname, dirEnv, "assets","images","favicon.ico")))
 app.use(/.*\.map$/i, function (req, res) {
   res.sendStatus(404)
 })
