e1b6fbbc856a81d43b3f00d16bbe5aa82b3f7e6c
Marcos J. Alvarez <garusis@gmail.com>
2017-03-17 03:10:58 -0500

    Add hasOne relation

--- development/modules/instructors/servicesInstructors.js
+++ development/modules/instructors/servicesInstructors.js
@@ -7,14 +7,14 @@
  */
 ;
 (function (module) {
-  InstructorService.$inject = ['Instructor', "$q", "$http", "ROUTES", "originsManager", "NewsService"];
-  function InstructorService (Instructor, $q, $http, ROUTES, originsManager, NewsService) {
+  InstructorService.$inject = ["Instructor", "Relbui", "$q", "$http", "ROUTES", "originsManager", "NewsService"];
+  function InstructorService (Instructor, Relbui, $q, $http, ROUTES, originsManager, NewsService) {
 
     this.login = function ($user) {
       return Instructor.login($user).$promise
     }
 
-    this.getCurrent = function (force) {
+    this.getCurrent = function () {
       return Instructor.getCurrent().$promise
         .then(processUser)
     }
@@ -26,7 +26,9 @@
     function processUser (instructor) {
       if (!instructor.__is_process__) {
         instructor.__is_process__ = true
-        instructor.coursesInstructor = new CourseInstructorRelation(instructor)
+        instructor.coursesInstructor = new Relbui.HasMany(
+          originsManager.getOrigin() + "/" + ROUTES.INSTRUCTORS.__BASE__ + "/" + instructor.id + "/" + ROUTES.INSTRUCTORS.IMAGE
+        )
         instructor.commentInstructor = new CommentInstructorRelation(instructor)
         instructor.image = new ImageInstructorRelation(instructor)
         instructor.image.get()
@@ -34,267 +36,11 @@
       return instructor
     }
 
-    function ImageInstructorRelation (instructor) {
-      this.basePath = originsManager.getOrigin() +
-        "/" + ROUTES.INSTRUCTORS.__BASE__ +
-        "/" + instructor.id +
-        "/" + ROUTES.INSTRUCTORS.IMAGE;
-    }
 
-    ImageInstructorRelation.prototype = new Object();
-
-    ImageInstructorRelation.prototype.get = function () {
-      var relation = this
-      return $http.get(this.basePath)
-        .then(function (response) {
-          _.assign(relation, response.data)
-          return relation
-        })
-    }
-
-    function CommentInstructorRelation (instructor) {
-      this.basePath = originsManager.getOrigin() +
-        "/" + ROUTES.INSTRUCTORS.__BASE__ +
-        "/" + instructor.id +
-        "/" + ROUTES.INSTRUCTORS.COMMENTS.__BASE__;
-
-    }
-
-    CommentInstructorRelation.prototype = new Array();
-
-    CommentInstructorRelation.prototype.__process__ = function (comment) {
-      comment.publication = new PublicationCommentRelation(comment, this)
-      return comment
-    }
-
-    CommentInstructorRelation.prototype.__addToCache__ = function (comment) {
-      this.push(this.__process__(comment))
-    }
-
-    CommentInstructorRelation.prototype.get = function (filter) {
-      if (!filter) {
-        filter = {}
-      }
-      if (!filter.include) {
-        filter.include = []
-      }
-
-      var relation = this
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          _.forEach(response.data, relation.__addToCache__.bind(relation))
-          return response.data
-        })
-    }
-
-    function PublicationCommentRelation (comment, commentInstructorRelation) {
-      this.publication_type = comment.publication_type
-
-      if (this.publication_type == "Publication") {
-        this.basePath = originsManager.getOrigin() + "/" + ROUTES.PUBLICATIONS.__BASE__
-      } else {
-        this.basePath = originsManager.getOrigin() + "/" + ROUTES.VIDEOS.__BASE__
-      }
-      this.basePath += "/" + comment.publication_id
-    }
-
-    PublicationCommentRelation.prototype = new Object()
-
-    PublicationCommentRelation.prototype.load = function (filter) {
-      if (!filter) {
-        filter = {}
-      }
-      if (!filter.include) {
-        filter.include = []
-      }
-
-      var relation = this;
-      var promise
-      if (this.publication_type == "Publication") {
-        promise = this.loadAsPublication(filter)
-      } else {
-        promise = this.loadAsVideo(filter)
-      }
-      promise
-        .then(function (response) {
-          _.assign(relation, response.data)
-          return response.data
-        })
-        .catch(function (response) {
-          if (response.status == 404) {
-            relation.id = null
-          }
-          throw response
-        })
-      return promise
-    }
-
-    PublicationCommentRelation.prototype.loadAsPublication = function (filter) {
-      filter.include.push({relation: "instructor"})
-      return $http.get(this.basePath, {params: {filter: filter}})
-    }
-
-    PublicationCommentRelation.prototype.loadAsVideo = function (filter) {
-      filter.include.push({
-        relation: "module",
-        scope: {
-          include: "course"
-        }
-      })
-      return $http.get(this.basePath, {params: {filter: filter}})
-    }
-
-    function CourseInstructorRelation (instructor) {
-      this.basePath = originsManager.getOrigin() +
-        "/" + ROUTES.INSTRUCTORS.__BASE__ +
-        "/" + instructor.id +
-        "/" + ROUTES.INSTRUCTORS.COURSES_INSTRUCTOR.__BASE__
-    }
-
-    CourseInstructorRelation.prototype = new Array()
-
-    CourseInstructorRelation.prototype.__addToCache__ = function (module) {
-      this.push(this.__process__(module))
-    }
-
-    CourseInstructorRelation.prototype.__process__ = function (courseInstructor) {
-      courseInstructor.modules = new ModuleRelation(courseInstructor, this)
-      return courseInstructor
-    }
-
-    CourseInstructorRelation.prototype.getById = function (courseId) {
-      var relation = this
-      var filter = {
-        where: {
-          course_id: courseId
-        }
-      }
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          return relation.__process__(response.data[0])
-        })
-    }
-
-    CourseInstructorRelation.prototype.get = function (filter) {
-      var relation = this
-      if (!filter) {
-        filter = {}
-      }
-      if (!filter.include) {
-        filter.include = [
-          {
-            relation: "course",
-            scope: {
-              include: ["instructor", "image"]
-            }
-          }
-        ]
-      }
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          relation.length = 0
-          _.forEach(response.data, function (course) {
-            relation.__addToCache__(course)
-          })
-        });
-    }
-
-    ModuleRelation.prototype = new Array()
-    function ModuleRelation (courseInstructor, courseInstructorRelation) {
-      this.basePath = courseInstructorRelation.basePath + "/" + courseInstructor.id + "/" + ROUTES.INSTRUCTORS.COURSES_INSTRUCTOR.MODULES
-    }
-
-    ModuleRelation.prototype.__addToCache__ = function (module) {
-      this.push(this.__process__(module))
-    }
-
-    ModuleRelation.prototype.__process__ = function (module) {
-      var videos = module.videos;
-      var videoRelation = new VideoRelation(module, this)
-      videos.forEach(videoRelation.__addToCache__.bind(videoRelation))
-      module.videos = videoRelation
-      return module
-    }
-
-    ModuleRelation.prototype.get = function (filter) {
-      var relation = this
-      if (!filter) {
-        filter = {}
-      }
-      if (!filter.include) {
-        filter.include = []
-      }
-
-      filter.include.push("videos")
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          relation.length = 0
-          response.data.forEach(relation.__addToCache__.bind(relation))
-          return relation
-        })
-    }
-
-    function VideoRelation (module, moduleRelation) {
-      this.basePath = originsManager.getOrigin() + "/" + ROUTES.VIDEOS.__BASE__
-    }
-
-    VideoRelation.prototype = new Array()
-
-    VideoRelation.prototype.__process__ = function (video) {
-      video.comments = new CommentRelation(video, this)
-      return video
-    }
-
-    VideoRelation.prototype.__addToCache__ = function (video) {
-      this.push(this.__process__(video))
-    }
-
-    VideoRelation.prototype.get = function (filter) {
-      var relation = this
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          relation.length = 0
-          response.data.forEach(relation.__addToCache__.bind(relation))
-          return relation
-        })
-    }
-
-    function CommentRelation (video, videoRelation) {
-      this.basePath = videoRelation.basePath + "/" + video.id + "/" + ROUTES.VIDEOS.COMMENTS
-    }
-
-    CommentRelation.prototype = new Array()
-
-    CommentRelation.prototype.__process__ = function (video) {
-      return video
-    }
-
-    CommentRelation.prototype.__addToCache__ = function (video) {
-      this.push(this.__process__(video))
-    }
-
-    CommentRelation.prototype.get = function (filter) {
-      var relation = this
-      return $http.get(this.basePath, {params: {filter: filter}})
-        .then(function (response) {
-          relation.length = 0
-          response.data.forEach(relation.__addToCache__.bind(relation))
-          return relation
-        })
-    }
-
-    CommentRelation.prototype.create = function (data) {
-      var relation = this
-      return $http.post(this.basePath, data)
-        .then(function (response) {
-          relation.__addToCache__(response.data)
-          return data
-        })
-    }
 
   }
 
-  module.service('InstructorService', InstructorService)
-})(angular.module('jg.marlininternacional.instructors'));
+  module.service("InstructorService", InstructorService)
+})(angular.module("jg.marlininternacional.instructors"));
 
 
--- development/modules/utilities/servicesUtilities.js
+++ development/modules/utilities/servicesUtilities.js
@@ -10,20 +10,127 @@
 ;
 (function (module) {
 
-  function RelbuiProvider () {
+  HasOneRelation.prototype = new Array()
+  function HasManyRelation ($http) {
+    this.$http = $http
+  }
 
+  /**
+   * @param {HasOneRelation} relation
+   * @param {String} basePath
+   * @param {Object} settings
+   * @param {Function} [settings.instanceCtor] Object constructor that receives each created or loaded element through the
+   *                    this relation. The new Object will be stored in the collection.
+   * @param {Function} [settings.afterLoad] function called before a new object in the array is created or loaded.
+   *                    This functions is called once for each element.
+   */
+  HasManyRelation.build = function (relation, basePath, settings) {
+    relation.basePath = basePath
+    relation.__settings__ = settings
+  }
+
+  HasManyRelation.prototype.__addToCache__ = function (instance) {
+    if (this.__settings__.instanceCtor) {
+      instance = new this.__settings__.instanceCtor(instance)
+    }
+    if (this.__settings__.afterLoad) {
+      this.__settings__.afterLoad(instance)
+    }
+    this.push(instance)
+  }
+
+  HasManyRelation.prototype.get = function (filter) {
+    var relation = this
+
+    return this.$http.get(this.basePath, {params: {filter: filter}})
+      .then(function (response) {
+        relation.length = 0
+        response.data.forEach(function (elem) {
+          relation.__addToCache__(elem)
+        })
+        return relation
+      })
+  }
+
+  function HasOneRelation ($http, basePath, settings) {
+    this.$http = $http
+    HasOneRelation.build(this, basePath, settings)
+  }
+
+  /**
+   * @param {HasOneRelation} relation
+   * @param {String} basePath
+   * @param {Object} settings
+   * @param {Function} [settings.instanceCtor] Object constructor that receives each created or loaded element through the
+   *                    this relation. The new Object will be stored in the collection.
+   * @param {Function} [settings.afterLoad] function called before a new object in the array is created or loaded.
+   *                    This functions is called once for each element.
+   */
+  HasOneRelation.build = function (relation, basePath, settings) {
+    relation.basePath = basePath
+    relation.__settings__ = settings
+  }
+
+  HasOneRelation.prototype.__load__ = function (instance) {
+    if (this.__settings__.instanceCtor) {
+      instance = new this.__settings__.instanceCtor(instance)
+    }
+    this.__proto__.__proto__.__proto__ = instance
+    if (this.__settings__.afterLoad) {
+      this.__settings__.afterLoad(this)
+    }
+  }
+
+  HasOneRelation.prototype.get = function (filter) {
+    var relation = this
+
+    return this.$http.get(this.basePath, {params: {filter: filter}})
+      .then(function (response) {
+        relation.__load__(response.data)
+        return relation
+      })
+  }
+
+  function RelbuiProvider () {
 
     this.$get = ["$http", function ($http) {
 
-      function HasMany () {
+      /**
+       *
+       * @param {String} basePath
+       * @param {Object} settings
+       * @param {Function} [settings.instanceCtor] Object constructor that receives each created or loaded element through
+       *                    this relation. The new Object will be stored in the collection.
+       * @param {Function} [settings.afterLoad] function called before a new object in the array is created or loaded.
+       *                    This functions is called once for each element.
+       */
+      function HasMany (basePath, settings) {
+        HasManyRelation.build(this, basePath, settings)
       }
 
+      HasMany.prototype = new HasManyRelation($http)
+
+      /**
+       *
+       * @param {String} basePath
+       * @param {Object} settings
+       * @param {Function} [settings.instanceCtor] Object constructor that receives the created or loaded element through
+       *                    this relation.                     .
+       * @param {Function} [settings.afterLoad] function called before a new object in the array is created or loaded.
+       *                    This functions is called once for each element.
+       */
+      function HasOne (basePath, settings) {
+        this.__proto__ = new HasOneRelation($http, basePath, settings)
+      }
+
+      /**
+       * @class Relbui
+       */
       return {
-        HasMany : HasMany
+        HasMany: HasMany,
+        HasOne: HasOne
       }
-
     }]
-
 
   }
 
