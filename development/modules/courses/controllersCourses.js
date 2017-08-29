7ce60c7cd79bb55c7478a3800d18d71cae2b3f47
diegoparada <diegoarmandoparada@hotmail.com>
2017-08-29 11:07:59 -0500

    #71 The course selection field is loading the courses correctly

--- development/modules/courses/controllersCourses.js
+++ development/modules/courses/controllersCourses.js
@@ -132,6 +132,49 @@
     $scope.loadCourse();
   }
 
+    CourseInfoRequestController.$inject = ["$scope", "CourseService", "Contact"]
+    function CourseInfoRequestController ($scope, CourseService, Contact) {
+        var contactCtrl = this;
+
+        contactCtrl.vm = {
+            data: {},
+            courses: [],
+            success: false,
+            error: false
+        }
+
+        CourseService.loadCourses()
+            .then(function (courses) {
+                contactCtrl.vm.courses = courses
+            })
+
+        contactCtrl.submit = function (data, course) {
+            var sendData = _.clone(data)
+            if (course) {
+                sendData.subject = "Solicitud de información sobre el curso " + course.name
+                sendData.body = "Solicito información sobre el curso " + course.name
+            }
+            Contact.create(sendData)
+                .$promise
+                .then(function () {
+                    contactCtrl.vm.data.toName = ""
+                    contactCtrl.vm.data.to = ""
+                    contactCtrl.vm.selectedCourse = null
+                    contactCtrl.vm.success = true
+                    contactCtrl.vm.error = false
+                    $scope.contactForm.$setPristine()
+                    $scope.contactForm.$setUntouched()
+                })
+                .catch(function () {
+                    contactCtrl.vm.success = false
+                    contactCtrl.vm.error = true
+                })
+        }
+
+    }
+
+
   module.controller('ListCourseController', ListCourseController)
-    .controller('ShowCourseController', ShowCourseController);
+      .controller('CourseInfoRequestController', CourseInfoRequestController)
+      .controller('ShowCourseController', ShowCourseController);
 })(angular.module('jg.marlininternacional.courses'));
